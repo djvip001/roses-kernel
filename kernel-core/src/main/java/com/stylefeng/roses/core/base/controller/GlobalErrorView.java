16408d5e4a2d2dc25b5186f4d1e538e9ed241bed
dengjinhmm <956454822@qq.com>
2019-02-11 17:35:12 +0800

    1

--- kernel-core/src/main/java/com/stylefeng/roses/core/base/controller/GlobalErrorView.java
+++ kernel-core/src/main/java/com/stylefeng/roses/core/base/controller/GlobalErrorView.java
@@ -15,14 +15,16 @@
  */
 package com.stylefeng.roses.core.base.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.stylefeng.roses.core.reqres.response.ErrorResponseData;
-import com.stylefeng.roses.kernel.model.exception.enums.CoreExceptionEnum;
-import org.springframework.web.servlet.View;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+
+import org.springframework.web.servlet.View;
+
+import com.alibaba.fastjson.JSON;
+import com.stylefeng.roses.core.reqres.response.ErrorResponseData;
+import com.stylefeng.roses.kernel.model.exception.enums.CoreExceptionEnum;
 
 /**
  * 错误页面的默认跳转(例如请求404的时候,默认走这个视图解析器)
@@ -32,26 +34,31 @@ import java.util.Map;
  */
 public class GlobalErrorView implements View {
 
-    @Override
-    public String getContentType() {
-        return "text/html";
-    }
+	@Override
+	public String getContentType() {
+		return "text/html";
+	}
 
-    @Override
-    public void render(Map<String, ?> map, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
-        httpServletResponse.setCharacterEncoding("utf-8");
-        httpServletResponse.setContentType("application/json");
+	@Override
+	public void render(Map<String, ?> map, HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse) throws Exception {
+		httpServletResponse.setCharacterEncoding("utf-8");
+		httpServletResponse.setContentType("application/json");
 
-        if (map != null && map.get("code") != null && map.get("message") != null) {
-            httpServletResponse.getWriter().write(JSON.toJSONString(new ErrorResponseData((Integer) map.get("code"), (String) map.get("message"))));
-        } else {
-            if (map != null && map.get("status") != null && map.get("error") != null) {
-                Object status = map.get("status");
-                Object error = map.get("error");
-                httpServletResponse.getWriter().write(JSON.toJSONString(new ErrorResponseData((Integer) status, (String) error)));
-            } else {
-                httpServletResponse.getWriter().write(JSON.toJSONString(new ErrorResponseData(CoreExceptionEnum.PAGE_NULL.getCode(), CoreExceptionEnum.PAGE_NULL.getMessage())));
-            }
-        }
-    }
+		if (map != null && map.get("code") != null && map.get("message") != null) {
+			httpServletResponse.getWriter().write(
+					JSON.toJSONString(new ErrorResponseData((Integer) map.get("code"), (String) map.get("message"))));
+		} else {
+			if (map != null && map.get(" status") != null && map.get("error") != null) {
+				Object status = map.get("status");
+				Object error = map.get("error");
+				httpServletResponse.getWriter()
+						.write(JSON.toJSONString(new ErrorResponseData((Integer) status, (String) error)));
+			} else {
+				httpServletResponse.getWriter()
+						.write(JSON.toJSONString(new ErrorResponseData(CoreExceptionEnum.PAGE_NULL.getCode(),
+								CoreExceptionEnum.PAGE_NULL.getMessage())));
+			}
+		}
+	}
 }
